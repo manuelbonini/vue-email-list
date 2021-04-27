41db063323e8412f545f853e7c35dffcb52bbe34
manuel <manuelbonini17@gmail.com>
2021-04-27 16:32:37 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -2,32 +2,23 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            arrayLength: 10,
             mail: []
         },
         methods: {
-            arrayPush() {
-
-            }
+            
         },
         mounted() {
-            axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    const result = response.data;
-                    console.log(result);
+            for( let i=0 ; i < this.arrayLength ; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        const result = response.data;
+                        console.log(result);
 
-                    this.mail.push(result.response);
-                    console.log(this.mail);
-                });
-            axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    const result = response.data;
-                    console.log(result);
-
-                    this.mail.push(result.response);
-                    console.log(this.mail);
-                });
+                        this.mail.push(result.response);
+                    });
+            }
         }
     }
 );
